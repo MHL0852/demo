13d8843af6a2c6b3ea3350f40964159e81fe9710
haila.mo <haila.mo@56linked.com>
2018-05-17 10:58:53 +0800

    添加

--- 表格列宽拖拽/test.js
+++ 表格列宽拖拽/test.js
@@ -4,6 +4,17 @@
         containerWidth,
         width,
         endX;
+    /*let getTop = function (dom, data) {
+        console.dir(dom);
+        if (dom.offsetParent.nodeName === 'BODY') {
+            data.top += dom.offsetTop;
+            return data
+        } else {
+            data.top += dom.offsetTop;
+            return getTop(dom.offsetParent, data)
+        }
+    };*/
+
     let headContent = document.getElementById('tabHeader').rows[0].cells;
     [].forEach.call(headContent,item=>{
         let line = document.createElement('div');
@@ -13,7 +24,6 @@
 
     let downFn = function (e) {
         e = e || window.event;
-
         if (e.target.className === 'line') {
             initX = e.clientX;
             width = getComputedStyle(e.target.parentNode).width;
@@ -23,18 +33,25 @@
             containerWidth = parseFloat(getComputedStyle(e.target.parentNode.parentNode.parentNode.parentNode).width)
         }
     };
+
     let moveFn = function (e) {
         e = e || window.event;
-        endX = e.clientX;
         if (initEvent) {
-            if (Math.abs(endX - initX) >= 10) {
+            endX = e.clientX;
+            console.log(e);
+            initEvent.target.style.position='fixed';
+            initEvent.target.style.top=getTop(initEvent.target,{top:0}).top+'px';
+            initEvent.target.style.left=e.screenX+'px';
+            initEvent.target.style.background='#000';
+            initEvent.target.style.border='1px solid #ccc';
+          /*  if (Math.abs(endX - initX) >= 10) {
                 if ((initEvent.target.parentNode.clientWidth === 352) && (endX - initX <= 0)) {
                     return
                 }
                 initEvent.target.parentNode.width = Math.round(width + endX - initX);
                 initEvent.target.parentNode.parentNode.parentNode.parentNode.style.width = Math.round(containerWidth + endX - initX) + 'px';
-            }
-            endX = null;
+            }*/
+            // endX = null;
         }
     };
 
@@ -47,17 +64,17 @@
                     initEvent = null;
                     initX = null;
                     endX = null;
-                    window.addEventListener('mousemove', moveFn, false);
+                    window.removeEventListener('mousemove', moveFn, false);
                     return
                 }
                 initEvent.target.parentNode.width = Math.round(width + endX - initX);
+                initEvent.target.parentNode.parentNode.parentNode.parentNode.style.width = Math.round(containerWidth + endX - initX) + 'px';
             }
             initEvent = null;
             initX = null;
             endX = null;
-            window.addEventListener('mousemove', moveFn, false)
+            window.removeEventListener('mousemove', moveFn, false)
         }
-
     };
     window.addEventListener('mousedown', downFn, false);
 
